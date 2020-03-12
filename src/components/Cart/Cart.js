a56a125806af95c90ea80a44df397c4e0cc108c8
shoaib6174 <shoaib6174@gmail.com>
2020-03-13 03:09:46 +0600

    hi

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+    console.log(cart)
+    const totalPrice= cart.reduce((total,prd) => total + prd.price,0);
+    
+
+    return (
+        <div>
+            <h4>Order Summary</h4>
+            <p>Items Ordered: {cart.length}</p>
+            <p>Total Price: {totalPrice}</p>
+        </div>
+    );
+};
+
+export default Cart;
